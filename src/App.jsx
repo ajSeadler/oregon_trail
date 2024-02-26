69503a00acc0df8fcaa1aac4423337b4d22ef7b4
ajSeadler <aj6287@gmail.com>
2024-02-26 14:58:14 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Oregon Trail Game</h1>
+      {/* Components for game screens will go here */}
+    </div>
+  );
+}
+
+export default App;
